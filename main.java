c9c4b682ce15d10abfe6d5b55321f8400ceffc6c
zanejakobsone <144245679+zanejakobsone@users.noreply.github.com>
2024-04-03 19:19:27 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,27 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {    
+   
+    double temp = -15.0;
+
+    if (temp <= 5) {
+      System.out.println("Wear super warm");
+    }
+    else if (temp <= 15) {
+      System.out.println("Wear warm");
+    }
+    else if (temp <= 30) {
+      System.out.println("Wear normal");
+    }
+    else {
+      System.out.println("You need cooling");}
+    }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
